316f8ba4e6c81319713e3ddbd697683693825e82
shafe <alam.shafee@gmail.com>
2026-04-09 12:54:37 +0600

    added auth view model

--- app/src/main/java/com/example/locationtracking/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/locationtracking/viewmodel/AuthViewModel.kt
@@ -1,9 +1,26 @@
 package com.example.locationtracking.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.locationtracking.repo.UserRepository
 
 class AuthViewModel(private val repo: UserRepository) : ViewModel() {
 
+    val loginResult = MutableLiveData<Pair<Boolean, String?>>()
+    val registerResult = MutableLiveData<Pair<Boolean, String?>>()
 
+
+    fun login(email: String, password: String) {
+
+        repo.loginUser(email, password) { success, message ->
+            loginResult.postValue(success to message)
+        }
+    }
+
+    fun register(email: String, password: String) {
+        repo.registerUser(email, password) { success, message ->
+            registerResult.postValue(success to message)
+        }
+
+    }
 }
